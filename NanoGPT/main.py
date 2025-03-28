080f0dfc067662a1cd7a6032e5b72b745424640e
csk7 <csivanitw@gmail.com>
2025-03-28 07:58:42 -0400

    test function working

--- NanoGPT/main.py
+++ NanoGPT/main.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 # 0. Constants
-batch_size = 4
-block_size = 8
-embedding_dim = 32
-max_iters = 1000
-learning_rate = 1e-2
+batch_size = 8
+block_size = 32
+embedding_dim = 128
+max_iters = 20000
+test_iters = 1000
+learning_rate = 3e-4
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 1. Get the data
 with open('data/input.txt', 'r', encoding='utf-8') as file:
@@ -70,6 +71,19 @@ model = model.to(device)
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
 
 #Test loop
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(test_iters)
+        for k in range(test_iters):
+            xb, yb = get_batch(split)
+            _, loss = model(xb, yb)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 
 #Training loop
@@ -79,8 +93,9 @@ for iter in range(max_iters):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    if(iter % 100 == 0):
-        print(f"iter {iter}: loss {loss.item()}")       
+    if(iter % 2000 == 0):
+        curLoss = estimate_loss()
+        print(f"iter {iter}: Train loss {curLoss['train']}, Val loss {curLoss['val']}")       
 
 #Generate
 context = torch.zeros((1,1), device=device, dtype=torch.long)
